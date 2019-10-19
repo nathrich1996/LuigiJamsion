2dff398c99c67f61e0bc18a2accfeeae7dc1fa10
CazaresJMH50 <CazaresJMH50@csu.fullerton.edu>
2019-10-19 13:36:35 -0700

    enemy gets dead

--- KillBox/Assets/Scripts/Player/PlayerControllerTemplate.cs
+++ KillBox/Assets/Scripts/Player/PlayerControllerTemplate.cs
@@ -13,6 +13,9 @@ public class PlayerControllerTemplate : MonoBehaviour
     Rigidbody2D rb;
     SpriteRenderer sprite;
     Animator anim;
+
+    int x_input;
+    int y_input;
     // Use this for initialization
     void Start()
     {
@@ -41,29 +44,35 @@ public class PlayerControllerTemplate : MonoBehaviour
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
             if (!facing_right)
                 Flip();
+            x_input = 1;
         }
         else if (Input.GetAxisRaw("Horizontal") < -threshold)
         {
             rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
             if (facing_right)
                 Flip();
+            x_input = -1;
         }
         else
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
+            x_input = 0;
         }
 
         if (Input.GetAxisRaw("Vertical") > threshold)
         {
             rb.velocity = new Vector2(rb.velocity.x, moveSpeed);
+            y_input = 1;
         }
         else if (Input.GetAxisRaw("Vertical") < -threshold)
         {
             rb.velocity = new Vector2(rb.velocity.x, -moveSpeed);
+            y_input = -1;
         }
         else
         {
             rb.velocity = new Vector2(rb.velocity.x, 0);
+            y_input = 0;
         }
         //For setting rotation
         if (facing_right)
@@ -79,5 +88,11 @@ public class PlayerControllerTemplate : MonoBehaviour
     void Flip()
     {
         facing_right = !facing_right;
+    }
+
+    public Vector2 GetDirection()
+    {
+        Vector2 vector = new Vector2(x_input, y_input);
+        return vector;
     }
 }
